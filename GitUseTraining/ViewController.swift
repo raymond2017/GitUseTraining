295071b47cd34c3c189b6a694cf8ac70781896a1
admin <admin@scotttao-chs.zh.wistronits.com>
2019-04-10 11:04:03 +0800

    Add button to view and related event

--- GitUseTraining/ViewController.swift
+++ GitUseTraining/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Starting app...")
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +22,11 @@ class ViewController: NSViewController {
         }
     }
 
-
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("Button clicked!")
+    }
+    
+    
 }
 
