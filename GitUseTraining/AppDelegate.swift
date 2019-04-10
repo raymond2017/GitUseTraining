02011d348267ad556865f416b4f777814aa0c0f3
admin <admin@scotttao.zh.wistronits.com>
2019-04-10 10:34:45 +0800

    Initial Commit

---
+++ GitUseTraining/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitUseTraining
+//
+//  Created by admin on 2019/4/10.
+//  Copyright © 2019 raymond. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitUseTraining/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitUseTraining
+//
+//  Created by admin on 2019/4/10.
+//  Copyright © 2019 raymond. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
