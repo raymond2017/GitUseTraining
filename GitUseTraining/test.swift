178932ec2d6ca5a7f4bfccd96d9794fa6e901f73
Yang <admin@scotttao-chs.zh.wistronits.com>
2019-04-11 11:00:51 +0800

    add test.swift locally

--- GitUseTraining/test.swift
+++ GitUseTraining/test.swift
@@ -1,1 +1,11 @@
-hello world
+//
+//  test.swift
+//  GitUseTraining
+//
+//  Created by admin on 2019/4/11.
+//  Copyright © 2019 raymond. All rights reserved.
+//
+
+import Foundation
+
+let name="raymond"
