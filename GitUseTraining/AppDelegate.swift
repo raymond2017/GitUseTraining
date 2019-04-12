53c50481b41e25d7a34bcd87c9184a39b94070c4
yjz1106 <yjz1106@163.com>
2019-04-12 15:17:11 +0800

    change again

--- GitUseTraining/AppDelegate.swift
+++ GitUseTraining/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        print("change from chris")
+        print("change from chris,再改一下")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
